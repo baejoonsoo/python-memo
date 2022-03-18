a4eed1d5a553dcfaba3ae23f20d8945be2ccd730
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-18 12:09:02 +0900

    문자열 관련 함수

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-americano=2500
-latte=3000
-cappuccino=3000
+strN="0123456"
 
-
-americanoNumber=int(input('americano를 얼마나 사실건가요? : '))
-latteNumber=int(input('latte를 얼마나 사실건가요? : '))
-cappuccinoNumber=int(input('cappuccino를 얼마나 사실건가요? : '))
-
-money= int(input('돈을 넣어주세요!! : '))
-
-needfulMoney= (americano*americanoNumber)+(latte*latteNumber)+(cappuccino*cappuccinoNumber)
-
-if needfulMoney>money:
-  print(f'돈이 부족합니다!!\n부족한 금액 :{needfulMoney-money}')
-else:
-  returnMoney= format((money-needfulMoney), ',')
-  print(f'잔돈은 ₩{returnMoney} 입니다')
+print(strN[-1:])
