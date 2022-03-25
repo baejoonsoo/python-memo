83c5b8bb3f363318ee42c03d881fb938a2204dd7
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-25 12:05:27 +0900

    컴퓨터가 정한 숫자 찾기

--- findNumber.py
+++ findNumber.py
@@ -1,18 +1,14 @@
 import random
 
-computerNumber = random.randint(0,100)
-attempts = 0
 
-while True:
+for i in range(10):
+  computerNumber = random.randint(1,5)
   userNum=int(input('컴퓨터가 정한 숫자를 찾아보세요!! -> '))
-  attempts+=1
 
   if userNum==computerNumber:
-    print(f'컴퓨터가 정한 숫자는 {computerNumber}였습니다!!\n 찾기까지 걸린 횟수 : {attempts}')
+    print('찾음')
     exit()
   else:
-    if userNum>computerNumber:
-      print('컴퓨터의 숫자 보다 큽니다!!\n')
-    else:
-      print('컴퓨터의 숫자 보다 작습니다!!\n')
-    
+    continue
+
+print('기회 끝')
