5b7a6e8098bc1fdd2d990bbe9a33bd7d989c5b3b
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-25 12:06:16 +0900

    숫자 예측기

--- findNumber.py
+++ findNumber.py
@@ -1,14 +1,18 @@
 import random
 
+computerNumber = random.randint(0,100)
+attempts = 0
 
-for i in range(10):
-  computerNumber = random.randint(1,5)
+while True:
   userNum=int(input('컴퓨터가 정한 숫자를 찾아보세요!! -> '))
+  attempts+=1
 
   if userNum==computerNumber:
-    print('찾음')
+    print(f'컴퓨터가 정한 숫자는 {computerNumber}였습니다!!\n 찾기까지 걸린 횟수 : {attempts}')
     exit()
   else:
-    continue
-
-print('기회 끝')
+    if userNum>computerNumber:
+      print('컴퓨터의 숫자 보다 큽니다!!\n')
+    else:
+      print('컴퓨터의 숫자 보다 작습니다!!\n')
+    
