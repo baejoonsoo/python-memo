17bc8fefa76b9ec0467eaff36f57ad9dc06d3ee0
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-03-25 11:11:35 +0900

    for문

--- calculator.py
+++
@@ -1,8 +0,0 @@
-n1, n2= map(int, input().split())
-
-print(f'{n1} + {n2} = {n1+n2}')
-print(f'{n1} - {n2} = {n1-n2}')
-print(f'{n1} x {n2} = {n1*n2}')
-print(f'{n1} ÷ {n2} = {n1/n2}')
-print(f'{n1} % {n2} = {n1%n2}')
-print(f'{n1} ^ {n2} = {n1**n2}')
--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import random
+sum =0
 
-chose=input("가위, 바위, 보 ==>")
+for num in range(1001,2000,2):
+  sum+=num
 
-
-botChose=random.choice(['가위','바위','보'])
-
-if (chose=="가위" and botChose=="보") or (chose=="바위" and botChose=="가위") or (chose=="보" and botChose=="바위"):
-  print('이김')
-elif (chose==botChose) or (chose== botChose) or (chose==botChose):
-  print('비김')
-else:
-  print('짐')
-
+print(sum)
