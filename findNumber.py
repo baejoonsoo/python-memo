7d0e06b85ae95e7b82fe0ac0633393a5acffe766
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-01 10:45:45 +0900

    숫자 찾기 - 과제임

--- findNumber.py
+++ findNumber.py
@@ -1,17 +1,42 @@
 import random
 
-computerNumber = random.randint(0,100)
-attempts = 0
+attempts = 5
+
+userName=input('네 이름은 뭐니?\n')
+print(f'반가워. {userName}. 내가 1부터 30사이의 수를 가지고 있어.\n맞춰봐')
 
 while True:
-  userNum=int(input('컴퓨터가 정한 숫자를 찾아보세요!! -> '))
-  attempts+=1
+  computerNumber = random.randint(1,30)
+  gameClear=False
 
-  if userNum==computerNumber:
-    print(f'컴퓨터가 정한 숫자는 {computerNumber}였습니다!!\n 찾기까지 걸린 횟수 : {attempts}')
-    exit()
-  else:
-    if userNum>computerNumber:
-      print('컴퓨터의 숫자 보다 큽니다!!\n')
+  print(f'\n\n{attempts}번 안에 맞혀야 돼')
+
+  for i in range(attempts):
+    userNum=int(input('추측한 숫자를 입력하세요->'))
+
+    if userNum==computerNumber:
+      print(f'{i+1}번만에 맞췄어요!! 축하해요')
+      gameClear=True
+      break
+    elif userNum >30 or userNum<1:
+      print('...내가 분명 1부터 30사이라고 했을텐데?')
+    elif userNum > computerNumber:
+      print('추측한 숫자가 컴퓨터가 가진 숫자보다 커요')
     else:
-      print('컴퓨터의 숫자 보다 작습니다!!\n')
+      print('추측한 숫자가 컴퓨터가 가진 숫자보다 작아요')
+  
+  isTry= input('\n\n게임을 다시 할까요?(YES or NO) : ')
+  
+  if isTry=='YES':
+    if gameClear:
+      attempts-=1
+    else:
+      attempts+=1
+    
+    continue
+  elif isTry=="NO":
+    break
+  else:
+    print('허용되지 않는 입력')
+    exit()
+    
