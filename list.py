8f0e8d20edc1cd10e8dd2d9c76627cf1600bfcb3
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-08 10:21:24 +0900

    튜플

--- list.py
+++ list.py
@@ -1,10 +1,27 @@
-num=list()
 
-for i in range(5):
-  num.append(int(input('평가 점수 : ')))
+import random
 
-sum=0
-for n in num:
-  sum+=n
+winCount=[0,0,0]
 
-print(sum/len(num))
+for i in range(10000):
+  com1= random.choice(['가위','바위','보'])
+  com2= random.choice(['가위','바위','보'])
+
+  # com1 win
+  if (com1=='가위' and com2=='보') or (com1=='바위' and com2=='가위') or (com1=='보' and com2=='바위'):
+    winCount[0]+=1
+  elif (com2=='가위' and com1=='보') or (com2=='바위' and com1=='가위') or (com2=='보' and com1=='바위'):
+    #com2 win
+    winCount[1]+=1
+  else:
+    winCount[2]+=1
+
+
+if winCount[0]>winCount[1]:
+  print('com1 win!!')
+elif winCount[0]<winCount[1]:
+  print('com2 win!!')
+else:
+  print('no winner')
+
+print(winCount)
---
+++ tuple.py
@@ -0,0 +1,6 @@
+myTuple=(1,2,3)
+myList=list(myTuple)
+myList.append(4)
+myTuple=tuple(myList)
+
+print(myTuple)
