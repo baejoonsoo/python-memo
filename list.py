b43df0f69f811b170c92e44b74989cde4cef2622
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-08 09:47:24 +0900

    .

--- list.py
+++ list.py
@@ -1,27 +1,10 @@
-myList = [30,10,20]
-print(f'현재 리스트 : {myList}')
+num=list()
 
-myList.append(40)
-print(f'append(40) 후의 리스트 : {myList}')
+for i in range(5):
+  num.append(int(input('평가 점수 : ')))
 
-print(f'pop()으로 추출한 값 : {myList.pop()}')
-print(f'pop() 후의 리스트 : {myList}')
+sum=0
+for n in num:
+  sum+=n
 
-myList.sort()
-print(f'sort() 후의 리스트 {myList}')
-
-myList.reverse()
-print(f'reverse() 후의 리스트 {myList}')
-
-print(f'20값의 위치 : {myList.index(20)}')
-
-myList.insert(2,222)
-print(f'insert(2,222) 후의 리스트 : {myList}')
-
-myList.remove(222)
-print(f'remove(222) 후의 리스트 : {myList}')
-
-myList.extend([77,88,77])
-print(f'extend[77,88,77]) 후의 리스트 : {myList}')
-
-print(f"77값의 개수 : {myList.count(77)}")
+print(sum/len(num))
